da697842cdb468d3b76bbfbf90424dc8569cf3af
Sdanielgg <40220220@esmad.ipp.pt>
2026-01-19 16:16:42 +0000

    Novo user e começo da implementação da api

---
+++ src/components/ui/chart/SunPosApi.vue
@@ -0,0 +1,27 @@
+<!-- Api sobre a posição do sol de cada dia apartir da latiude e longitude  -->
+<template>
+    <div>
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>    
