43ea4ca34990a52528104443681cfae1e5e071e0
Sdanielgg <40220220@esmad.ipp.pt>
2026-01-25 15:16:22 +0000

    Reapply "Merge branch 'main' of https://github.com/Laxingo/slumbr"
    This reverts commit ef0a74f99c3a714c14cd60b742619aad4835ee2a.

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -19,7 +19,7 @@
       <div class="dash_layout">
         <section class="dash_left">
           <div class="dash_card">
-            <UserPanel :metrics="panelMetrics" />
+            <SleepInfo :sleepData="sleepData" :metrics="panelMetrics"/>
           </div>
 
           <div class="dash_card">
@@ -33,7 +33,7 @@
           </div>
         </section>
 
-        <aside class="dash_right">
+        <!-- <aside class="dash_right">
           <div class="dash_card">
             <div class="dash_card_top">
               <h2 class="dash_card_title">Logs</h2>
@@ -51,15 +51,11 @@
               </div>
             </div>
 
-            <div class="table_wrap">
-              <SleepTable
-                :sleepData="filteredSleepData"
-                @edit="openEdit"
-                @delete="deleteLog"
-              />
-            </div>
-          </div>
-        </aside>
+           <div class="table_wrap">
+              <SleepTable :sleepData="filteredSleepData" @edit="openEdit" @delete="deleteLog" />
+            </div> --> 
+          <!-- </div>
+        </aside> -->
       </div>
 
       <!-- Modal -->
@@ -138,11 +134,9 @@ import { useAuthStore } from '@/stores/auth'
 import UserPanel from '@/components/ui/UserPanel.vue'
 import QuestList from '@/components/ui/QuestList.vue'
 import SunPosApi from '@/components/Api/SunPosApi.vue'
-
-const API_BASE = 'http://127.0.0.1:3000'
-
+import SleepInfo from '@/components/ui/sleepInfo.vue'
 export default {
-  components: { AppHeader, SleepTable, SleepChart, UserPanel, QuestList, SunPosApi },
+  components: { AppHeader, SleepTable, SleepChart, UserPanel, QuestList, SunPosApi, SleepInfo },
 
   data() {
     return {
@@ -168,7 +162,7 @@ export default {
       const auth = useAuthStore()
       return auth.userId
     },
-
+    
     filteredSleepData() {
       const sorted = [...this.sleepData].sort((a, b) => (b.date || 0) - (a.date || 0))
       if (this.limit === 0) return sorted
@@ -259,10 +253,9 @@ export default {
 
   methods: {
     async welcomeUser() {
-      if (!this.userId) return
-      const res = await fetch(`${API_BASE}/users?id=${encodeURIComponent(this.userId)}`)
-      const data = await res.json()
-      this.userName = data?.[0]?.userName || ''
+      const res = await fetch(`http://localhost:3000/users?id=${this.userId}`)
+      this.data = await res.json()
+      this.userName = this.data[0].userName
     },
 
     async fetchSleepData() {
